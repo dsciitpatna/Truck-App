553074d4080368d06f753a75721f33146c78933f
Oindrila Bhadra <oindrilabhadra78@gmail.com>
2019-07-20 02:35:37 +0530

    Add home activity

---
+++ app/src/main/java/com/example/truck_app/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.truck_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
