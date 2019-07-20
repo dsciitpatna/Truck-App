4e189e9cb8bb7e2624e22241aa5e4e84e16f2fe1
Amartya Mondal <hayyoulistentome@gmail.com>
2019-07-20 18:25:30 +0530

    feat: Map showing

--- app/src/main/java/com/example/truck_app/Home/HomeActivity.kt
+++ app/src/main/java/com/example/truck_app/Home/HomeActivity.kt
@@ -5,9 +5,20 @@ import android.os.Bundle
 import com.example.truck_app.Home.contract.IHomePresenter
 import com.example.truck_app.Home.contract.IHomeView
 import com.example.truck_app.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class HomeActivity : AppCompatActivity(), IHomeView {
+
+class HomeActivity : AppCompatActivity(), IHomeView, OnMapReadyCallback {
+
+
     private lateinit var homePresenter: IHomePresenter
+    private lateinit var map: GoogleMap
+    private lateinit var mapFragment: SupportMapFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +26,33 @@ class HomeActivity : AppCompatActivity(), IHomeView {
         homePresenter = HomePresenter(this)
         homePresenter.onAttach(this)
 
+       // val mapReadyCallback = MapReadyCallback()
+
+        //mapFragment = supportFragmentManager.findFragmentById(R.id.show_map) as SupportMapFragment
+        //mapFragment.getMapAsync(mapReadyCallback)
+
+       val  mapFragment=(supportFragmentManager.findFragmentById(R.id.show_map) as SupportMapFragment?)?.let {
+            it.getMapAsync(this)
+        }
+    }
+
+//    internal inner class MapReadyCallback: OnMapReadyCallback{
+//        override fun onMapReady(googleMap: GoogleMap?) {
+//            map = googleMap as GoogleMap
+//
+//            // Add a marker in Sydney and move the camera
+//            val sydney = LatLng(-34.0, 151.0)
+//            map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+//            map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+//        }
+//
+//    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        val sydney = LatLng(-34.0, 151.0)
+        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 
 }
